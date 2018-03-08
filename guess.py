f0aed37de5512b0ad6035f92727c4b61eec01f5e
Jalonzpa <joshuaal22@g.eips.ca>
2018-03-07 18:27:39 -0700

    New game

--- guess.py
+++ guess.py
@@ -1,6 +1,18 @@
 #!/usr/local/bin/python3
-from random import random.randint
+from random import randint
+number = randint(0, 50)
 
-number = randint(0, 100)
+print("Guess the number between 1 and 50!")
 
+while True:
+    guess = int(input())
 
+    if guess > 50:
+        print("It's between 1 and 50. Too high!")
+    if guess == number:
+        print("Right on!")
+	break
+    elif guess < number:
+        print("It is higher than %s.") % guess
+    elif guess > number:
+        print("It's lower than %s.") % guess
